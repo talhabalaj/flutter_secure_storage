d0c5832e0b7b102fb7479d13ca2afa7f624d83f7
Talha Balaj <talhabalaj@hotmail.com>
2021-01-11 15:56:42 +0500

    some progress

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -4,6 +4,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_secure_storage/flutter_secure_storage_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterSecureStoragePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStoragePlugin"));
 }
--- windows/flutter_secure_storage_plugin.cpp
+++ windows/flutter_secure_storage_plugin.cpp
@@ -2,6 +2,7 @@
 
 // This must be included before many other Windows headers.
 #include <windows.h>
+#include <wincred.h>
 
 // For getPlatformVersion; remove unless needed for your plugin implementation.
 #include <VersionHelpers.h>
@@ -14,68 +15,119 @@
 #include <memory>
 #include <sstream>
 
-namespace {
+namespace
+{
 
-class FlutterSecureStoragePlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+  class FlutterSecureStoragePlugin : public flutter::Plugin
+  {
+  public:
+    static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  FlutterSecureStoragePlugin();
+    FlutterSecureStoragePlugin();
 
-  virtual ~FlutterSecureStoragePlugin();
+    virtual ~FlutterSecureStoragePlugin();
 
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-};
+  private:
+    // Called when a method is called on this plugin's channel from Dart.
+    void HandleMethodCall(
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
-// static
-void FlutterSecureStoragePlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "flutter_secure_storage",
-          &flutter::StandardMethodCodec::GetInstance());
+    static const char* label;
+  };
 
-  auto plugin = std::make_unique<FlutterSecureStoragePlugin>();
+  // static
+  void FlutterSecureStoragePlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarWindows *registrar)
+  {
+    auto channel =
+        std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+            registrar->messenger(), "plugins.it_nomads.com/flutter_secure_storage",
+            &flutter::StandardMethodCodec::GetInstance());
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
+    auto plugin = std::make_unique<FlutterSecureStoragePlugin>();
 
-  registrar->AddPlugin(std::move(plugin));
-}
+    channel->SetMethodCallHandler(
+        [plugin_pointer = plugin.get()](const auto &call, auto result) {
+          plugin_pointer->HandleMethodCall(call, std::move(result));
+        });
 
-FlutterSecureStoragePlugin::FlutterSecureStoragePlugin() {}
-
-FlutterSecureStoragePlugin::~FlutterSecureStoragePlugin() {}
-
-void FlutterSecureStoragePlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
+    registrar->AddPlugin(std::move(plugin));
+  
   }
-}
 
-}  // namespace
+  FlutterSecureStoragePlugin::FlutterSecureStoragePlugin() {
+  }
+
+  FlutterSecureStoragePlugin::~FlutterSecureStoragePlugin() {}
+
+  void FlutterSecureStoragePlugin::HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+  {
+    const flutter::EncodableValue *args = method_call.arguments();
+    const flutter::EncodableMap *argsMap = std::get_if<flutter::EncodableMap>(args);
+
+    const std::string *key, *value;
+
+    try {
+      const auto keyEncoded = argsMap->at(flutter::EncodableValue("key"));
+      key = std::get_if<std::string>(&keyEncoded);
+    } catch(std::out_of_range) {
+      key = NULL;
+    }
+
+    try {
+      const auto valueEncoded = argsMap->at(flutter::EncodableValue("value"));
+      value = std::get_if<std::string>(&valueEncoded);
+    } catch(std::out_of_range) {
+      value = NULL;
+    }
+      
+    if (method_call.method_name().compare("read") == 0)
+    {
+      PCREDENTIALW cred = { 0 };
+      if (!key) {
+        result->Error("Bad arguments", "Key is required to read a value");
+      }
+
+      if(!CredRead(LPWSTR(L""), CRED_TYPE_GENERIC, 0, &cred)) {
+
+      }
+
+    }
+    else if (method_call.method_name().compare("readAll") == 0)
+    {
+     
+    }
+    else if (method_call.method_name().compare("write") == 0)
+    {
+       if (!key || !value) {
+        result->Error("Bad arguments", "Key and value are required to write a value");
+      }
+    }
+    else if (method_call.method_name().compare("delete") == 0)
+    {
+       if (!key) {
+        result->Error("Bad arguments", "Key is required to delete a value");
+      } 
+    }
+    else if (method_call.method_name().compare("deleteAll") == 0)
+    {
+    }
+    else
+    {
+      result->NotImplemented();
+    }
+
+    result->Success();
+  }
+
+} // namespace
 
 void FlutterSecureStoragePluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
+    FlutterDesktopPluginRegistrarRef registrar)
+{
   FlutterSecureStoragePlugin::RegisterWithRegistrar(
       flutter::PluginRegistrarManager::GetInstance()
           ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
